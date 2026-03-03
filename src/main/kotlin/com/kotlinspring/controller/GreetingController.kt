e6969b40fe6b57dbaa59d4bcbfd28fe4e3447f32
Weble21 <89642807+Weble21@users.noreply.github.com>
2026-03-03 23:40:54 +0900

    greeting controller & Service

--- src/main/kotlin/com/kotlinspring/controller/GreetingController.kt
+++ src/main/kotlin/com/kotlinspring/controller/GreetingController.kt
@@ -1,16 +1,18 @@
 package com.kotlinspring.controller
 
+import com.kotlinspring.service.GreetingService
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequestMapping("/v1/greeting")
-class GreetingController {
+@RequestMapping("/v1/greetings")
+class GreetingController(val greetingService: GreetingService) {
 
     @GetMapping("/{name}")
     fun retrieveGreeting(@PathVariable("name") name: String) : String{
-        return "Hello $name"
+        //return "Hello $name"
+        return greetingService.retrieveGreeting(name)
     }
 }
