5cf0991577e64d99962e13f3fa2fe1284a15ad6e
Weble21 <89642807+Weble21@users.noreply.github.com>
2026-03-11 22:48:18 +0900

    POST endpoint for Course & intg test

---
+++ src/main/kotlin/com/kotlinspring/entity/Course.kt
@@ -0,0 +1,17 @@
+package com.kotlinspring.entity
+
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.Table
+
+@Entity
+@Table(name = "Courses")
+data class Course (
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id : Int?,
+    val name : String,
+    val category : String
+)
